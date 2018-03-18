9595a74c0c72fa056ba3fd4eb5300dd66106f380
Joey Gibson <joey@joeygibson.com>
2018-03-18 00:36:57 -0400

    modules

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,15 +1,8 @@
-mod network {
-    fn connect() {}
-}
+// If a module named foo has no submodules, you should put the declarations
+// for foo in a file named foo.rs.
+// If a module named foo does have submodules, you should put the declarations
+// for foo in a file named foo/mod.rs.
 
-mod client {
-    fn connect() {}
-}
+mod client;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod network;
