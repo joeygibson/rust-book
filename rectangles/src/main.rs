c96bdc3628a0258c35c606d7c362a62c99e2f17c
Joey Gibson <joey@joeygibson.com>
2018-03-17 23:03:04 -0400

    associated functions

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,19 +4,45 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // associated functions are like static methods
+    fn square(size: u32) -> Rectangle {
+        Rectangle{width: size, height: size}
+    }
+}
+
 fn main() {
     let rect = Rectangle{width: 30, height: 50};
 
     println!(
         "Area of rectangle is {} square pixels",
-        area(&rect)
+        rect.area()
     );
 
     // :? for basic Debug output
     // :#? for formatted Debug output
     println!("R: {:#?}", rect);
-}
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
+    let rect1 = Rectangle{width: 10, height: 10};
+
+    println!("r1 > r? {}", rect1.can_hold(&rect));
+    println!("r > r1? {}", rect.can_hold(&rect1));
+
+    println!();
+
+    let square = Rectangle::square(23);
+    println!(
+        "Area of square is {} square pixels",
+        square.area()
+    );
+    
+    println!("Square: {:#?}", square);
 }
