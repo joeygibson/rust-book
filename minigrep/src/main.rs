876f42b50d9024cadd4e71619f4d9229c45c5326
Joey Gibson <joey@joeygibson.com>
2018-03-22 00:04:10 -0400

    better modulariazion and error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,20 +1,55 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_name = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_name);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("processing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let mut f = File::open(file_name).expect("file not found");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_name);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+impl Config {
+    // `&'static str` is the type and lifetime of string literals
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+
+        Ok(Config {
+            query,
+            file_name,
+        })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.file_name)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("reading file");
+    f.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
