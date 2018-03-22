df5c0aca35b8fbedf547e6d4695c25769608d689
Joey Gibson <joey@joeygibson.com>
2018-03-21 23:02:54 -0400

    Moar testing

--- communicator/src/main.rs
+++
@@ -1,8 +0,0 @@
-extern crate communicator;
-
-// This pattern is quite common for executable projects: most functionality is
-// in a library crate, and the binary crate uses that library crate. As a result,
-// other programs can also use the library crate, and it’s a nice separation of concerns.
-fn main() {
-    communicator::client::connect();
-}
