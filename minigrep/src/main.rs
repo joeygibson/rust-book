42172a7af60dacbef4f646b90f44153276a7ab4e
Joey Gibson <joey@joeygibson.com>
2018-03-22 23:06:05 -0400

    Added basic search function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,12 +8,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("processing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_name);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
