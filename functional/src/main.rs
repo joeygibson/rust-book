108bbbd7d79effd0c6925a9c32768b1bc3ac9025
Joey Gibson <joey@joeygibson.com>
2018-03-25 22:59:37 -0400

    custom iterators

--- functional/src/main.rs
+++ functional/src/main.rs
@@ -29,13 +29,30 @@ impl<T> Cacher<T>
 }
 
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+//    let simulated_user_specified_value = 10;
+//    let simulated_random_number = 7;
+//
+//    generate_workout(
+//        simulated_user_specified_value,
+//        simulated_random_number,
+//    );
 
-    generate_workout(
-        simulated_user_specified_value,
-        simulated_random_number,
-    );
+    // iterator adapters
+    let v1 = vec![1, 2, 3];
+    let res: Vec<_> = v1.iter().map(|x| x + 23).collect();
+    println!("Res: {:?}", res);
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter()
+        .filter(|s| s.size == shoe_size)
+        .collect()
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
@@ -67,17 +84,76 @@ fn generate_workout(intensity: u32, random_number: u32) {
     }
 }
 
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-    
+
     #[test]
-    fn call_with_different_values() {
-        let mut c = Cacher::new(|a| a);
+    fn call_custom_iterator() {
+        let c = Counter::new();
 
-        let v1 = c.value(1);
-        let v2 = c.value(2);
+        for i in c.into_iter() {
+            println!("C: {}", i);
+        }
 
-        assert_eq!(v2, 2);
+        let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
+
+        assert_eq!(18, sum);
+    }
+
+//    #[test]
+//    fn call_with_different_values() {
+//        let mut c = Cacher::new(|a| a);
+//
+//        let v1 = c.value(1);
+//        let v2 = c.value(2);
+//
+//        assert_eq!(v2, 2);
+//    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 13, style: String::from("sandal") },
+            Shoe { size: 10, style: String::from("boot") },
+        ];
+
+        let in_my_size = shoes_in_my_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe { size: 10, style: String::from("sneaker") },
+                Shoe { size: 10, style: String::from("boot") },
+            ]
+        );
     }
 }
