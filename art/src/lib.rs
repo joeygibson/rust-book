3c125c4eea49449d3cd306ffc6488cef004353e9
Joey Gibson <joey@joeygibson.com>
2018-03-26 23:19:09 -0400

    re-exporting

--- art/src/lib.rs
+++ art/src/lib.rs
@@ -2,5 +2,8 @@
 //!
 //! A library for modeling artistic concepts.
 
+pub use kinds::PrimaryColor;
+pub use utils::mix;
+
 pub mod kinds;
 pub mod utils;
--- art/src/utils.rs
+++ art/src/utils.rs
@@ -2,4 +2,6 @@ use kinds::*;
 
 /// Combines two primary colors in equal amounts to create a
 /// secondary color.
-pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {}
+pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+    SecondaryColor::Purple
+}
