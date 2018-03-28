5231e247d9c724ce0e779827d5137045da143201
Joey Gibson <joey@joeygibson.com>
2018-03-27 23:33:58 -0400

    loops

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -18,4 +18,20 @@ fn main() {
     for el in (1..4).rev() {
         println!("{}!", el);
     }
+
+    // while let
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top)
+    }
+
+    let v = vec![1, 2, 3];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("Val at {} is {}", index, value);
+    }
 }
