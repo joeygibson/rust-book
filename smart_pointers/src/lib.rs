7c06e43eb7466c60036aed412bcb6f44f0a98d47
Joey Gibson <joey@joeygibson.com>
2018-03-27 22:35:04 -0400

    done with smart pointers

--- smart_pointers/src/lib.rs
+++ smart_pointers/src/lib.rs
@@ -1,9 +1,16 @@
 use std::ops::Deref;
+use std::rc::Rc;
 
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+#[derive(Debug)]
+enum List2 {
+    Cons2(i32, Rc<List2>),
+    Nil2,
 }
 
 //fn main() {
@@ -44,6 +51,16 @@ fn hello(word: &str) -> String {
     format!("Hello, {}", word)
 }
 
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("dropping custom smart pointer: `{}`", self.data)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -74,4 +91,24 @@ mod tests {
         assert_eq!("Hello, World", hello(s));
         assert_eq!("Hello, World", hello(&m));
     }
+
+    #[test]
+    fn testDrop() {
+        let c = CustomSmartPointer { data: String::from("String A") };
+        let d = CustomSmartPointer { data: String::from("String B") };
+
+        println!("CSPs created");
+    }
+
+    #[test]
+    fn testRc() {
+        use List2::*;
+
+        let a = Rc::new(Cons2(5, Rc::new(Cons2(10, Rc::new(Nil2)))));
+        let b = Cons2(3, Rc::clone(&a));
+        let c = Cons2(4, Rc::clone(&a));
+
+        println!("A: {:?}", a);
+    }
 }
+
