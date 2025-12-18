83c430c83e41f0316768b4d4d7b8518147c0033d
Joey Gibson <joey@joeygibson.com>
2025-12-18 17:22:29 -0500

    references

---
+++ playground/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+
+    change(&mut s1);
+    println!("The length of '{}' is {}.", s1, calculate_length(&s1));
+
+    let reference_to_nothing = dangle();
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn dangle() -> &String {
+    let s = String::from("dangle");
+    &s
+}
