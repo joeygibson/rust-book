973b6393cc3acadd635b639ec5c9f431c4258ca2
Joey Gibson <joey@joeygibson.com>
2025-12-18 22:58:15 -0500

    enums

---
+++ enums/src/main.rs
@@ -0,0 +1,28 @@
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
+
+fn main() {
+    let four = IpAddrKind::V4(127, 0, 0, 1);
+    let six = IpAddrKind::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let s = Some(23);
+
+}
