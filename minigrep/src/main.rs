b55532ca46c045f825a69734fe7a6e66f3fb72ca
Joey Gibson <joey@joeygibson.com>
2025-12-19 23:44:12 -0500

    minigrep - beginning

---
+++ minigrep/src/main.rs
@@ -0,0 +1,50 @@
+use std::{env, fs, process};
+use minigrep::search;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+    let contents =
+        fs::read_to_string(&config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = &args[1].clone();
+        let file_path = &args[2].clone();
+
+        Ok(Config {
+            query: query.to_string(),
+            file_path: file_path.to_string(),
+        })
+    }
+}
